a567b460544fc7e9269b964750690ea009cc4bdb
josephat <josephat94@gmail.com>
2018-09-05 15:18:28 -0500

    correction effects

--- js/scroll.js
+++ js/scroll.js
@@ -12,14 +12,14 @@ $(document).ready(function() {
     
     
                     if(windowHeight >= about  ){
-                        $('#services').fadeIn(500);
+                        
                         
                         $('#s1').fadeIn(500);
                         $('#s2').fadeIn(500);
                         $('#s3').fadeIn(500);
                         $('#s4').fadeIn(500);
     
-                    }else{$('#services').fadeOut(500);
+                    }
                         $('#s1').fadeOut(500);
                         $('#s2').fadeOut(500);
                         $('#s3').fadeOut(500);
